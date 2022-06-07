5b1c74447b7677ca738f1d3ba804e983ba169ccf
Andrew Gasser <agasser01@hotmail.com>
2022-06-07 13:14:08 -0500

    Add slew rate limits

--- src/main/java/frc/robot/commands/DefaultDriveCommand.java
+++ src/main/java/frc/robot/commands/DefaultDriveCommand.java
@@ -1,45 +1,56 @@
 package frc.robot.commands;
 
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.DrivetrainSubsystem;
 
-import java.util.function.DoubleSupplier;
-
 public class DefaultDriveCommand extends CommandBase {
-    private final DrivetrainSubsystem m_drivetrainSubsystem;
+  private final DrivetrainSubsystem m_drivetrainSubsystem;
 
-    private final DoubleSupplier m_translationXSupplier;
-    private final DoubleSupplier m_translationYSupplier;
-    private final DoubleSupplier m_rotationSupplier;
+  private final DoubleSupplier m_translationXSupplier;
+  private final DoubleSupplier m_translationYSupplier;
+  private final DoubleSupplier m_rotationSupplier;
 
-    public DefaultDriveCommand(DrivetrainSubsystem drivetrainSubsystem,
-                               DoubleSupplier translationXSupplier,
-                               DoubleSupplier translationYSupplier,
-                               DoubleSupplier rotationSupplier) {
-        this.m_drivetrainSubsystem = drivetrainSubsystem;
-        this.m_translationXSupplier = translationXSupplier;
-        this.m_translationYSupplier = translationYSupplier;
-        this.m_rotationSupplier = rotationSupplier;
+  // FIXME these limits are a guess
+  private final SlewRateLimiter translateXRateLimiter = new SlewRateLimiter(4);
+  private final SlewRateLimiter translateYRateLimiter = new SlewRateLimiter(4);
+  private final SlewRateLimiter rotationRateLimiter = new SlewRateLimiter(Math.PI);
 
-        addRequirements(drivetrainSubsystem);
-    }
+  /**
+   * Constructor
+   * @param drivetrainSubsystem drivetrain
+   * @param translationXSupplier supplier for translation X component, in meters per second
+   * @param translationYSupplier supplier for translation Y component, in meters per second
+   * @param rotationSupplier supplier for rotation component, in radians per second
+   */
+  public DefaultDriveCommand(DrivetrainSubsystem drivetrainSubsystem,
+      DoubleSupplier translationXSupplier,
+      DoubleSupplier translationYSupplier,
+      DoubleSupplier rotationSupplier) {
+    this.m_drivetrainSubsystem = drivetrainSubsystem;
+    this.m_translationXSupplier = translationXSupplier;
+    this.m_translationYSupplier = translationYSupplier;
+    this.m_rotationSupplier = rotationSupplier;
 
-    @Override
-    public void execute() {
-        // You can use `new ChassisSpeeds(...)` for robot-oriented movement instead of field-oriented movement
-        m_drivetrainSubsystem.drive(
-                ChassisSpeeds.fromFieldRelativeSpeeds(
-                        m_translationXSupplier.getAsDouble(),
-                        m_translationYSupplier.getAsDouble(),
-                        m_rotationSupplier.getAsDouble(),
-                        m_drivetrainSubsystem.getGyroscopeRotation()
-                )
-        );
-    }
+    addRequirements(drivetrainSubsystem);
+  }
 
-    @Override
-    public void end(boolean interrupted) {
-        m_drivetrainSubsystem.drive(new ChassisSpeeds(0.0, 0.0, 0.0));
-    }
+  @Override
+  public void execute() {
+    // You can use `new ChassisSpeeds(...)` for robot-oriented movement instead of field-oriented movement
+    m_drivetrainSubsystem.drive(
+        ChassisSpeeds.fromFieldRelativeSpeeds(
+            translateXRateLimiter.calculate(m_translationXSupplier.getAsDouble()),
+            translateYRateLimiter.calculate(m_translationYSupplier.getAsDouble()),
+            rotationRateLimiter.calculate(m_rotationSupplier.getAsDouble()),
+            m_drivetrainSubsystem.getGyroscopeRotation()));
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    m_drivetrainSubsystem.drive(new ChassisSpeeds(0.0, 0.0, 0.0));
+  }
 }

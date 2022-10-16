6e9aaa6ffca06a6fe7d418823a333900b9e78774
Andrew Gasser <agasser01@hotmail.com>
2022-10-16 18:52:13 -0500

    Working pose estimator
    Standard Deviation tuning is probably needed
    This trusts model and vision equally

--- src/main/java/frc/robot/subsystems/PoseEstimatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/PoseEstimatorSubsystem.java
@@ -30,21 +30,23 @@ public class PoseEstimatorSubsystem extends SubsystemBase {
 
   // Physical location of the camera on the robot, relative to the center of the
   // robot.
-  private static final Transform2d CAMERA_TO_ROBOT = new Transform2d(new Translation2d(0, 0), new Rotation2d(0));
+  private static final Transform2d CAMERA_TO_ROBOT = 
+      new Transform2d(new Translation2d(Units.inchesToMeters(12.75), 0.0), new Rotation2d(0.0));
 
   // Ordered list of target poses by ID (WPILib is adding some functionality for
   // this)
   private static final List<Pose2d> targetPoses = Collections.unmodifiableList(List.of(
-      new Pose2d(0, 0, Rotation2d.fromDegrees(0))));
+      new Pose2d(Units.inchesToMeters(84), Units.inchesToMeters(39.4375), Rotation2d.fromDegrees(180)),
+      new Pose2d(Units.inchesToMeters(84), 0.0, Rotation2d.fromDegrees(180))));
   
   // Kalman Filter Configuration. These can be "tuned-to-taste" based on how much
   // you trust your various sensors. Smaller numbers will cause the filter to
   // "trust" the estimate from that particular component more than the others. 
   // This in turn means the particualr component will have a stronger influence
   // on the final pose estimate.
-  private static final Matrix<N3, N1> stateStdDevs = VecBuilder.fill(0.05, 0.05, Units.degreesToRadians(5));
-  private static final Matrix<N1, N1> localMeasurementStdDevs = VecBuilder.fill(Units.degreesToRadians(0.1));
-  private static final Matrix<N3, N1> visionMeasurementStdDevs = VecBuilder.fill(0.01, 0.01, Units.degreesToRadians(0.1));
+  private static final Matrix<N3, N1> stateStdDevs = VecBuilder.fill(0.1, 0.1, Units.degreesToRadians(5));
+  private static final Matrix<N1, N1> localMeasurementStdDevs = VecBuilder.fill(Units.degreesToRadians(0.01));
+  private static final Matrix<N3, N1> visionMeasurementStdDevs = VecBuilder.fill(0.1, 0.1, Units.degreesToRadians(5));
   private final SwerveDrivePoseEstimator poseEstimator;
 
   private final Field2d field2d = new Field2d();
@@ -69,33 +71,38 @@ public class PoseEstimatorSubsystem extends SubsystemBase {
 
   @Override
   public void periodic() {
-    // Update pose estimator with drivetrain sensors
-    poseEstimator.update(drivetrainSubsystem.getGyroscopeRotation(), drivetrainSubsystem.getModuleStates());
-
     // Update pose estimator with visible targets
-    // var res = photonCamera.getLatestResult();
-    // if (res.hasTargets()) {
-    //   double imageCaptureTime = Timer.getFPGATimestamp() - res.getLatencyMillis();
+    var res = photonCamera.getLatestResult();
+    if (res.hasTargets()) {
+      double imageCaptureTime = Timer.getFPGATimestamp() - (res.getLatencyMillis() / 1000d);
 
-    //   for (PhotonTrackedTarget target : res.getTargets()) {
+      for (PhotonTrackedTarget target : res.getTargets()) {
 
-    //     var fiducialId = target.getFiducialId();
-    //     if (fiducialId >= 0 && fiducialId < targetPoses.size()) {
-    //       var targetPose = targetPoses.get(fiducialId);
+        var fiducialId = target.getFiducialId();
+        if (fiducialId >= 0 && fiducialId < targetPoses.size()) {
+          var targetPose = targetPoses.get(fiducialId);
 
-    //       Transform3d camToTarget = res.getBestTarget().getCameraToTarget();
-    //       var transform = new Transform2d(
-    //           camToTarget.getTranslation().toTranslation2d(),
-    //           camToTarget.getRotation().toRotation2d());
-    //       Pose2d camPose = targetPose.transformBy(transform.inverse());
-    //       poseEstimator.addVisionMeasurement(
-    //           camPose.transformBy(CAMERA_TO_ROBOT), imageCaptureTime);
-    //     }
-    //   }
-    // }
+          Transform3d camToTarget = target.getCameraToTarget();
+          var transform = new Transform2d(
+              camToTarget.getTranslation().toTranslation2d(),
+              camToTarget.getRotation().toRotation2d().minus(Rotation2d.fromDegrees(90)));
+
+          Pose2d camPose = targetPose.transformBy(transform.inverse());
+
+          var visionMeasurement = camPose.transformBy(CAMERA_TO_ROBOT);
+          field2d.getObject("MyRobot" + fiducialId).setPose(visionMeasurement);
+          // SmartDashboard.putString("Vision pose", String.format("(%.2f, %.2f) %.2f",
+          //   visionMeasurement.getTranslation().getX(),
+          //   visionMeasurement.getTranslation().getY(),
+          //   visionMeasurement.getRotation().getDegrees()));
+          poseEstimator.addVisionMeasurement(visionMeasurement, imageCaptureTime);
+        }
+      }
+          // Update pose estimator with drivetrain sensors
+    }
+    poseEstimator.updateWithTime(Timer.getFPGATimestamp(), drivetrainSubsystem.getGyroscopeRotation(), drivetrainSubsystem.getModuleStates());
 
     field2d.setRobotPose(getCurrentPose());
-
   }
 
   private String getFomattedPose() {
@@ -125,8 +132,9 @@ public class PoseEstimatorSubsystem extends SubsystemBase {
    * what "forward" is for field oriented driving.
    */
   public void resetFieldPosition() {
+    drivetrainSubsystem.resetGyroAngle();
     poseEstimator.resetPosition(
-      new Pose2d(getCurrentPose().getTranslation(), new Rotation2d()), drivetrainSubsystem.getGyroscopeRotation());
+      new Pose2d(), drivetrainSubsystem.getGyroscopeRotation());
   }
 
 }

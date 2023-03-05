5db17e4ab3c7e562d07bf4457cbabcb3164a1090
Andrew Gasser <agasser01@hotmail.com>
2023-03-04 20:12:03 -0600

    A little clean-up

--- src/main/java/frc/robot/commands/autonomous/DriveToPoseCommand.java
+++ src/main/java/frc/robot/commands/autonomous/DriveToPoseCommand.java
@@ -1,4 +1,4 @@
-package frc.robot.commands;
+package frc.robot.commands.autonomous;
 
 import static frc.robot.Constants.AutoConstants.THETA_kD;
 import static frc.robot.Constants.AutoConstants.THETA_kI;

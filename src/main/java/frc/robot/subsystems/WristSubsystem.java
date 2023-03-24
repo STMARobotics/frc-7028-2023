b6353184ad963e76e7458bca33ee7173b3984012
Andrew Gasser <agasser01@hotmail.com>
2023-03-24 14:45:53 -0500

    After practice 15
    Lower wrist limit
    Increase wrist pickup tolerance
    Rotate more toward target in 3 (no balance)

--- src/main/java/frc/robot/subsystems/WristSubsystem.java
+++ src/main/java/frc/robot/subsystems/WristSubsystem.java
@@ -33,7 +33,7 @@ public class WristSubsystem extends SubsystemBase {
   // Offset in rotations to add to encoder value - offset from arm horizontal to sensor zero
   private static final double ENCODER_OFFSET = -0.2285f;
   
-  private static final float LIMIT_BOTTOM = 0.15f;
+  private static final float LIMIT_BOTTOM = 0.12f;
   private static final float LIMIT_TOP = 0.4272f;
   private static final double LIMIT_TOP_RADIANS = Units.rotationsToRadians(LIMIT_TOP + ENCODER_OFFSET);
   private static final double LIMIT_BOTTOM_RADIANS = Units.rotationsToRadians(LIMIT_BOTTOM + ENCODER_OFFSET);
